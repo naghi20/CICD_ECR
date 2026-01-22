ecb9e0051296c0f68ea38fa07296949755553032
Ahmed AL Naghi <64667680+naghi20@users.noreply.github.com>
2026-01-22 15:27:47 +0300

    v2

--- app/app.py
+++ app/app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return "Automated CICD_ECR Deployment Working!", 200
 
-@app.route('/health')
-def health():
-    return "OK", 200
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
